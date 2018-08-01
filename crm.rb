c5ad3b5f84de845185f9fe30b1a69f05830070cd
curtischatch <curtis.chat.hk@gmail.com>
2018-08-01 19:21:17 -0400

    erb

--- crm.rb
+++ crm.rb
@@ -1,6 +1,18 @@
 require_relative 'contact'
 require 'sinatra'
 
+get '/' do
+  erb :index
+end
+
 get '/home' do
   erb :index
 end
+
+get '/contacts' do
+  erb :index
+
+
+after do
+  ActiveRecord::Base.connection.close
+ends
